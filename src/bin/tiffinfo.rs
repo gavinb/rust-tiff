6dace476dba057274fbfdc551e355cbbf1e18efc
Gavin Baker <gavinb@antonym.org>
2015-06-29 19:07:58 +1000

    Build fixes, clean build on 1.1

--- src/bin/tiffinfo.rs
+++ src/bin/tiffinfo.rs
@@ -1,19 +1,20 @@
 
-extern crate "rust-tiff" as tiff;
+extern crate rust_tiff as tiff;
 
-use std::os;
+use std::env;
 
 fn print_header(x: &tiff::TIFFHeader) {
-    println!("byte_order: {}", x.byte_order);
-    println!("magic:      {}", x.magic);
+    println!("byte_order: {:?}", x.byte_order);
+    println!("magic:      {:?}", x.magic);
     println!("ifd_offset: {}", x.ifd_offset);
 }
 
 fn main() {
     let tiff_reader = tiff::TIFFReader;
-    match tiff_reader.load(os::args()[1].as_slice()) {
+    let file_name = env::args().next().unwrap();
+    match tiff_reader.load(&file_name) {
         Ok(h) => {
-            println!("Read tiff {}", h);
+            println!("Read tiff {:?}", h);
             print_header(&*h);
         },
         Err(e) => {
